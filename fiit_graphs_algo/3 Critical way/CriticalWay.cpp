69c155ec7182d21e8fb34b73ecdbb8e21bc7d569
Ilia Yartsev <ilia.yartsev@gmail.com>
2020-05-26 23:31:59 +0300

    3 almost ready

--- fiit_graphs_algo/3 Critical way/CriticalWay.cpp
+++ fiit_graphs_algo/3 Critical way/CriticalWay.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -10,23 +11,36 @@ ofstream out("output.txt");
 
 
 
-void dfs (int v,vector<vector<int>> &g,vector<int> &ans,bool *used) {
-	*used[v] = true;
-	for (size_t i=0; i<g[v].size(); ++i) {
-		int to = g[v][i];
-		if (!*used[to])
-			dfs (to);
+void dfs (int start,vector<vector<int>> &g,vector<int> &ans,vector<bool> &used) {
+	used[start] = true;
+	for (size_t i=0; i<g[start].size(); ++i) {
+		//int to = g[start][i];
+		if (!used[i] && g[start][i]>=0){
+			// cout<<"test\n";
+			dfs (i,g,ans,used);
+		}
 	}
-	ans.push_back (v);
+	ans.push_back (start);
 }
 
-void topological_sort(vector<vector<int>> &g,vector<int> &ans,bool *used) {
-	for (int i=0; i<n; ++i)
-		*used[i] = false;
+void topological_sort(vector<vector<int>> &g,vector<int> &ans,vector<bool> &used) {
+	int n=g.size();
+	// cout<<"n= "<<n<<"\n";
+	for (size_t i=0; i<n; ++i){
+		used[i] = false;
+		// cout<<"used["<<i<<"]= "<<used[i]<<"\n";
+	}
 	ans.clear();
-	for (int i=0; i<n; ++i)
-		if (!*used[i])
-			dfs (i,g,ans,*used);
+
+	for (int i=0; i<n; ++i){
+		// cout<<used[i];
+		// cout<<"test2\n";
+		if (!used[i]){
+			// cout<<"test3\n";
+			dfs (i,g,ans,used);
+		}
+	}
+
 	reverse (ans.begin(), ans.end());
 }
 
@@ -37,16 +51,16 @@ int main(){
     {
       int n,i,j,z,min_int=-2147483647;
       in>>n;
-      cout<<"n= "<<n<<"\n";
+      // cout<<"n= "<<n<<"\n";
       vector<int> w;//Вектор весов
       vector<vector<int>> v(n,vector<int>());//Граф
-      vector<int> ans;
-      bool used[n];
+
+
 
       for(i=0;i<n;i++){//Считываем строку весов
         in>>j;
         w.push_back(j);
-        cout<<"Weight of "<<i<<" vertex is "<<j<<"\n";
+        // cout<<"Weight of "<<i<<" vertex is "<<j<<"\n";
       }
 
       for(i=0;i<n;i++){//Считываем дуги
@@ -56,26 +70,128 @@ int main(){
           if(z==1){
             v[i].push_back(w[i]);
             //cout<<"Exist from "<<i<<" to "<<j<<" weight "<<w[i]<<"\n";
-            cout<<w[i]<<" ";
+            // cout<<w[i]<<" ";
           }else{
-            v[i].push_back(min_int);
+            v[i].push_back(0);
             //cout<<"Doesn't Exist from "<<i<<" to "<<j<<" weight "<<0  <<"\n";
-            cout<<0<<" ";
+            // cout<<0<<" ";
           }
         }
-        cout<<"\n";
+        // cout<<"\n";
       }
 
+			// cout<<"Input done!\n";
       //Вывод
-      for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-          cout<<v[i][j]<<" ";
-        }
-        cout<<"\n";
-      }
+      // for(i=0;i<n;i++){
+      //   for(j=0;j<n;j++){
+      //     cout<<v[i][j]<<" ";
+      //   }
+      //   cout<<"\n";
+      // }
 
       //Topologic sort
+			vector<bool> used(n);
+			vector<int> ans(n);
+
+
+
       topological_sort(v,ans,used);
+			cout<<"Ans= ";
+			for (i=0;i<ans.size();i++){
+				cout<<ans[i];
+			}
+			cout<<"\n";
+
+			//Меняем строки в матрице в соответствии
+			//с проведённой топологической сортировкой
+			// for(i=0;i<v.size();i++)
+			// 	if (ans[i]!=i){
+			// 		for(j=0;j<v.size();j++){
+			// 			z=v[i][j];
+			// 			v[i][j]=v[ans[i]][j];
+			// 			v[ans[i]][j]=z;
+			// 			//И в векторе весов
+			// 			z=w[i];
+			// 			w[i]=w[ans[i]];
+			// 			w[ans[i]]=z;
+			//
+			//
+			// 		}
+			//
+			// 	}
+
+
+			//Меняем нули на min_int
+			for(i=0;i<v.size();i++)
+				for(j=0;j<v.size();j++)
+					if (v[i][j]==0)
+						v[i][j]=min_int;
+
+			//Вывод
+			// for(i=0;i<n;i++){
+			// 	for(j=0;j<n;j++){
+			// 		cout<<v[i][j]<<" ";
+			// 	}
+			// 	cout<<"\n";
+			// }
+
+			//Производим поиск критических путей
+			//vector<vector<int>> b(n,vector<int>(0));
+
+			for(i=0;i<n;i++){
+				for(j=i+1;j<n;j++)
+					for(z=j+1;z<n;z++)
+						if(v[i][z]<v[i][j]+v[j][z]){
+							v[i][z]=v[i][j]+v[j][z];
+							//b[i][z]=j;
+						}
+			}
+
+			//Вывод
+			// for(i=0;i<n;i++){
+			// 	for(j=0;j<n;j++){
+			// 		cout<<v[i][j]<<" ";
+			// 	}
+			// 	cout<<"\n";
+			// }
+
+			//Прибавляем вес конечных вершин
+			// for(i=0;i<v.size();i++)
+			// 	for(j=0;j<v.size();j++)
+			// 		if (v[i][j]>0)
+			// 			v[i][j]+=w[j];
+
+			// Вывод
+			// for(i=0;i<n;i++){
+			// 	for(j=0;j<n;j++){
+			// 		cout<<v[i][j]<<" ";
+			// 	}
+			// 	cout<<"\n";
+			// }
+
+
+			//Меняем  min_int  на нули
+			for(i=0;i<v.size();i++)
+				for(j=0;j<v.size();j++)
+					if (v[i][j]<0)
+						v[i][j]=0;
+
+			//Вывод
+			for(i=0;i<n;i++){
+				for(j=0;j<n;j++){
+					cout<<v[i][j]<<" ";
+				}
+				cout<<"\n";
+			}
+
+			//Вывод в файл
+			for(i=0;i<n;i++){
+				for(j=0;j<n;j++){
+					out<<v[i][j]<<" ";
+				}
+				out<<"\n";
+			}
+
 
 
 
